91f9390355592217e5bd54577ffbfa0bb7196282
边雷 <775884216@qq.com>
2017-01-01 00:50:50 +0800

    创建项目

---
+++ DrawingBoard/DrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingBoard
+//
+//  Created by 边雷 on 17/1/1.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingBoard/DrawingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingBoard
+//
+//  Created by 边雷 on 17/1/1.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
