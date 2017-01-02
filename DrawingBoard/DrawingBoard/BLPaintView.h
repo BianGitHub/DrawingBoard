375de0688f2ef8e150abc58ab6ee8bb6758c30cd
边雷 <775884216@qq.com>
2017-01-02 23:32:48 +0800

    创建类并关联

---
+++ DrawingBoard/DrawingBoard/BLPaintView.h
@@ -0,0 +1,13 @@
+//
+//  BLPaintView.h
+//  DrawingBoard
+//
+//  Created by 边雷 on 17/1/2.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLPaintView : UIView
+
+@end
---
+++ DrawingBoard/DrawingBoard/BLPaintView.m
@@ -0,0 +1,21 @@
+//
+//  BLPaintView.m
+//  DrawingBoard
+//
+//  Created by 边雷 on 17/1/2.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import "BLPaintView.h"
+
+@implementation BLPaintView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
